cdfb0a72fe3eb45730c34b4a71fc036f53941e1f
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2023-04-07 10:20:43 +0200

    startcommit

---
+++ src/bresenham.c
@@ -0,0 +1,339 @@
+//
+//  bresenham.c
+//  Mill32
+//
+//  Created by Ruedi Heimlicher on 02.08.2022.
+//  Copyright © 2022 Ruedi Heimlicher. All rights reserved.
+//
+
+#include "bresenham.h"
+#include <assert.h>
+//https://de.wikipedia.org/wiki/Bresenham-Algorithmus
+
+
+/* signum function */
+int sgn(int x)
+{
+    if (x > 0) return +1;
+    if (x < 0) return -1;
+    return 0;
+}
+
+
+void gbham(int xstart, int ystart, int xend, int yend)
+/*--------------------------------------------------------------
+ * Bresenham-Algorithmus: Linien auf Rastergeräten zeichnen
+ *
+ * Eingabeparameter:
+ *    int xstart, ystart        = Koordinaten des Startpunkts
+ *    int xend, yend            = Koordinaten des Endpunkts
+ *
+ * Ausgabe:
+ *    void SetPixel(int x, int y) setze ein Pixel in der Grafik
+ *         (wird in dieser oder aehnlicher Form vorausgesetzt)
+ *---------------------------------------------------------------
+ */
+{
+   int x, y, t, dx, dy, incx, incy, pdx, pdy, ddx, ddy, deltaslowdirection, deltafastdirection, err;
+   
+   /* Entfernung in beiden Dimensionen berechnen */
+   dx = xend - xstart;
+   dy = yend - ystart;
+   
+   /* Vorzeichen des Inkrements bestimmen */
+   incx = sgn(dx);
+   incy = sgn(dy);
+   if (dx < 0) dx = -dx;
+   if (dy < 0) dy = -dy;
+   
+   /* feststellen, welche Entfernung größer ist */
+   if (dx > dy)
+   {
+      /* x ist schnelle Richtung */
+      pdx = incx; pdy = 0;    //pd. ist Parallelschritt 
+      ddx = incx; ddy = incy; // dd. ist Diagonalschritt 
+      
+      deltaslowdirection = dy;   // Delta in langsamer Richtung 
+      deltafastdirection = dx;   // Delta in schneller Richtung
+   }
+   else
+   {
+      /* y ist schnelle Richtung */
+      pdx = 0;    pdy = incy; // pd. ist Parallelschritt 
+      ddx = incx; ddy = incy; // dd. ist Diagonalschritt 
+      
+      deltaslowdirection = dx;   // Delta in langsamer Richtung 
+      deltafastdirection = dy;   // Delta in schneller Richtung 
+   }
+   
+   /* Initialisierungen vor Schleifenbeginn */
+   x = xstart;
+   y = ystart;
+   err = deltafastdirection / 2;
+   setPixel(x,y);
+   
+   /* Pixel berechnen */
+   for(t = 0; t < deltafastdirection; ++t) /* t zaehlt die Pixel, deltafastdirection ist Anzahl der Schritte */
+   {
+      /* Aktualisierung Fehlerterm */
+      err -= deltaslowdirection;
+      if(err < 0)
+      {
+         /* Fehlerterm wieder positiv (>=0) machen */
+         err += deltafastdirection;
+         /* Schritt in langsame Richtung, Diagonalschritt */
+         x += ddx;
+         y += ddy;
+      }
+      else
+      {
+         /* Schritt in schnelle Richtung, Parallelschritt */
+         x += pdx;
+         y += pdy;
+      }
+      setPixel(x, y);
+   }
+}
+
+
+
+
+void
+plot_line (int x0, int y0, int x1, int y1)
+{
+   int dx =  abs(x1 - x0);
+   int sx = x0 < x1 ? 1 : -1;
+   int dy = -abs (y1 - y0);
+   int sy = y0 < y1 ? 1 : -1; 
+   int err = dx + dy; /* error value e_xy */
+   int e2;
+   for (;;){  /* loop */
+      setPixel (x0,y0);
+      if (x0 == x1 && y0 == y1) break; // am Ziel
+      e2 = 2 * err;
+      if (e2 >= dy) 
+      { 
+         err += dy; 
+         x0 += sx;          
+      } /* e_xy+e_x > 0 */
+      if (e2 <= dx) 
+      { 
+         err += dx; 
+         y0 += sy; 
+      } /* e_xy+e_y < 0 */
+   }
+}
+
+void
+move_line(int x1, int y1)
+{
+   int x0 = 0;
+   int y0 = 0;
+   int dx = x1;
+   int dy = y1;
+   
+   int err = dx + dy ;/* error value e_xy */
+   int e2;
+   for (;;){  /* loop */
+      {
+         if (x0 == x1 && y0 == y1)  // Ziel erreicht
+         {
+            
+            return;
+         }
+         e2 = 2 * err;
+         if (e2 >= dy) 
+         { 
+            err += dy; 
+            x0 += 1;  
+            // > schritt x 
+            
+         } /* e_xy+e_x > 0 */
+         if (e2 <= dx) 
+         { 
+            err += dx; 
+            y0 += 1; 
+            // > schritt y
+         } /* e_xy+e_y < 0 */
+      }
+   }
+}
+
+void
+plot_circle (int xm, int ym, int r)
+{
+   int x = -r, y = 0, err = 2-2*r; /* II. Quadrant */ 
+   do {
+      setPixel (xm-x, ym+y); /*   I. Quadrant */
+      setPixel (xm-y, ym-x); /*  II. Quadrant */
+      setPixel (xm+x, ym-y); /* III. Quadrant */
+      setPixel (xm+y, ym+x); /*  IV. Quadrant */
+      r = err;
+      if (r >  x) err += ++x*2+1; /* e_xy+e_x > 0 */
+      if (r <= y) err += ++y*2+1; /* e_xy+e_y < 0 */
+   } while (x < 0);
+}
+
+void
+plot_ellipse_rect (int x0, int y0, int x1, int y1)
+{
+   int a = abs (x1 - x0), b = abs (y1 - y0), b1 = b & 1; /* values of diameter */
+   long dx = 4 * (1 - a) * b * b, dy = 4 * (b1 + 1) * a * a; /* error increment */
+   long err = dx + dy + b1 * a * a, e2; /* error of 1.step */
+
+   if (x0 > x1) { x0 = x1; x1 += a; } /* if called with swapped points */
+   if (y0 > y1) y0 = y1; /* .. exchange them */
+   y0 += (b + 1) / 2;
+   y1 = y0-b1;   /* starting pixel */
+   a *= 8 * a; b1 = 8 * b * b;
+   do
+   {
+       setPixel (x1, y0); /*   I. Quadrant */
+       setPixel (x0, y0); /*  II. Quadrant */
+       setPixel (x0, y1); /* III. Quadrant */
+       setPixel (x1, y1); /*  IV. Quadrant */
+       e2 = 2 * err;
+       if (e2 >= dx)
+       {
+          x0++;
+          x1--;
+          err += dx += b1;
+       } /* x step */
+       if (e2 <= dy)
+       {
+          y0++;
+          y1--;
+          err += dy += a;
+       }  /* y step */ 
+   } while (x0 <= x1);
+   while (y0-y1 < b)
+   {  /* too early stop of flat ellipses a=1 */
+       setPixel (x0-1, y0); /* -> finish tip of ellipse */
+       setPixel (x1+1, y0++); 
+       setPixel (x0-1, y1);
+       setPixel (x1+1, y1--); 
+   }
+}
+
+void
+plot_basic_bezier (int x0, int y0, int x1, int y1, int x2, int y2)
+{                            
+  int sx = x0 < x2 ? 1 : -1;
+  int sy = y0 < y2 ? 1 : -1; /* step direction */
+  int cur = sx * sy *((x0 - x1) * (y2 - y1) - (x2 - x1) * (y0 - y1)); /* curvature */
+  int x = x0 - 2 * x1 + x2, y = y0 - 2 * y1 +y2, xy = 2 * x * y * sx * sy;
+                                /* compute error increments of P0 */
+  long dx = (1 - 2 * abs (x0 - x1)) * y * y + abs (y0 - y1) * xy - 2 * cur * abs (y0 - y2);
+  long dy = (1 - 2 * abs (y0 - y1)) * x * x + abs (x0 - x1) * xy + 2 * cur * abs (x0 - x2);
+                                /* compute error increments of P2 */
+  long ex = (1 - 2 * abs (x2 - x1)) * y * y + abs (y2 - y1) * xy + 2 * cur * abs (y0 - y2);
+  long ey = (1 - 2 * abs (y2 - y1)) * x * x + abs (x2 - x1) * xy - 2 * cur * abs (x0 - x2);
+                              /* sign of gradient must not change */
+  assert ((x0 - x1) * (x2 - x1) <= 0 && (y0 - y1) * (y2 - y1) <= 0); 
+  if (cur == 0)
+  { /* straight line */
+    plot_line (x0, y0, x2, y2);
+    return;
+  }
+  x *= 2 * x;
+  y *= 2 * y;
+  if (cur < 0)
+  { /* negated curvature */
+    x = -x;
+    dx = -dx;
+    ex = -ex;
+    xy = -xy;
+    y = -y;
+    dy = -dy;
+    ey = -ey;
+  }
+  /* algorithm fails for almost straight line, check error values */
+  if (dx >= -y || dy <= -x || ex <= -y || ey >= -x)
+  {        
+    plot_line (x0, y0, x1, y1); /* simple approximation */
+    plot_line (x1, y1, x2, y2);
+    return;
+  }
+  dx -= xy;
+  ex = dx + dy;
+  dy -= xy; /* error of 1.step */
+  for (;;)
+  { /* plot curve */
+    setPixel (x0, y0);
+    ey = 2 * ex - dy; /* save value for test of y step */
+    if (2 * ex >= dx)
+    { /* x step */
+      if (x0 == x2) break;
+      x0 += sx;
+      dy -= xy;
+      ex += dx += y; 
+    }
+    if (ey <= 0)
+    { /* y step */
+      if (y0 == y2) break;
+      y0 += sy;
+      dx -= xy;
+      ex += dy += x; 
+    }
+  }
+}  
+  
+
+/* EOF */
+
+/* EOF */
+
+
+// 'cx' and 'cy' denote the offset of the circle centre from the origin.
+void
+circle (int cx, int cy, int radius)
+{
+  int error = -radius;
+  int x = radius;
+  int y = 0;
+ 
+  // The following while loop may altered to 'while (x > y)' for a
+  // performance benefit, as long as a call to 'plot4points' follows
+  // the body of the loop. This allows for the elimination of the
+  // '(x != y') test in 'plot8points', providing a further benefit.
+  //
+  // For the sake of clarity, this is not shown here.
+  while (x >= y)
+  {
+    plot8points (cx, cy, x, y);
+    error += y;
+    ++y;
+    error += y;
+    // The following test may be implemented in assembly language in
+    // most machines by testing the carry flag after adding 'y' to
+    // the value of 'error' in the previous step, since 'error'
+    // nominally has a negative value.
+    if (error >= 0)
+    {
+      --x;
+      error -= x;
+      error -= x;
+    }
+  }
+}
+
+
+void
+plot8points (int cx, int cy, int x, int y)
+{
+  plot4points (cx, cy, x, y);
+  if (x != y) plot4points (cx, cy, y, x);
+}
+ 
+// The '(x != 0 && y != 0)' test in the last line of this function
+// may be omitted for a performance benefit if the radius of the
+// circle is known to be non-zero.
+void
+plot4points (int cx, int cy, int x, int y)
+{
+  setPixel (cx + x, cy + y);
+  if (x != 0) setPixel (cx - x, cy + y);
+  if (y != 0) setPixel (cx + x, cy - y);
+  if (x != 0 && y != 0) setPixel (cx - x, cy - y);
+}
+
---
+++ src/settings.h
@@ -0,0 +1,182 @@
+//
+//  settings.h
+//  Stepper32
+//
+//  Created by Ruedi Heimlicher on 07.05.2020.
+//  Copyright © 2020 Ruedi Heimlicher. All rights reserved.
+//
+
+#ifndef settings_h
+#define settings_h
+
+#define LOOPLED 13
+#define TIMER0_STARTWERT   0x40
+
+
+// Stepper A
+
+
+ // neu 200730
+ #define MC_STEP        14
+ #define MC_RI          15
+ #define MC_EN          16
+ 
+ //Pins 
+
+ #define MA_STEP         0
+ #define MA_RI           1
+ #define MA_EN           2
+ 
+// Stepper B
+ #define MB_STEP         3
+ #define MB_RI           4
+ #define MB_EN           5
+ 
+ #define END_A0_PIN      20           // Bit fuer Endanschlag bei A0
+ #define END_B0_PIN      21           // Bit fuer Endanschlag bei A1
+ 
+#define DC_PWM              22
+
+// 10,11,12: SPI
+ #define MD_STEP            14           // PIN auf Stepperport 2
+// 18, 19: I2C
+#define MD_RI              15
+#define MD_EN              16
+
+#define END_C0_PIN         11 //18           // Anschlagstatus:  Bit fuer Endanschlag bei C0
+#define END_D0_PIN         12 //19           // Anschlagstatus:  Bit fuer Endanschlag bei D0
+
+
+
+
+#define TASTE0            0   // HALT-Bit Motor A
+#define TASTE1            1
+
+
+#define STEPDUR 250
+
+#define STARTDELAYBIT       0
+#define HICOUNTBIT          1
+
+#define LOAD_NEXT          5  // Bit fuer Laden des naechsten Abschnitts in der loop
+#define LOAD_LAST          6  // Bit fuer Laden des letzten Abschnitts in der loop
+
+// auf Stepperport 1
+#define END_A0          4       //  Bit fuer Endanschlag A0 
+#define END_B0          5       //           Endanschlag B0 
+
+
+// Auf Stepperport 2
+#define END_C0          6       //  Bit fuer Endanschlag C0 
+#define END_D0          7       //           Endanschlag D0 
+
+
+#define RICHTUNG_A   0
+#define RICHTUNG_B   1
+#define RICHTUNG_C   2
+#define RICHTUNG_D   3
+
+#define MOTOR_A 0
+#define MOTOR_B 1
+#define MOTOR_C 2
+#define MOTOR_D 3
+
+
+#define HALT_PIN           0
+
+#define COUNT_A            0 // 4      // Motorstatus:   Schritte von Motor A zaehlen
+#define COUNT_B            1 // 5      // Motorstatus:   Schritte von Motor B zaehlen
+
+#define COUNT_C            2 // 4      // Motorstatus:   Schritte von Motor C zaehlen
+#define COUNT_D            3 // 5      // Motorstatus:   Schritte von Motor D zaehlen
+
+#define COUNT_END          6
+#define COUNT_LAST         7
+
+
+#define TIMER_ON           1 // Bit fuer timerfunktion start
+
+#define DC                  7    // DC ON: LO
+#define STROM               4    // Stepperstrom ON: HI
+
+#define GO_HOME            3     // Bit fuer befehl beginn home auf cncstatus
+#define DC_DIVIDER         1      // teilt die pwm-Frequenz in ISR
+
+
+
+
+// Ringbuffer
+#define RINGBUFFERTIEFE    4
+#define READYBIT           0        // buffer kann Daten aufnehmen
+#define FULLBIT            1        // Buffer ist voll
+#define STARTBIT           2        // Buffer ist geladen
+#define RINGBUFFERBIT      3        // Ringbuffer wird verwendet
+#define LASTBIT            4        // Letzter Abschnitt  ist geladen
+#define ENDBIT             5        // Letzter Abschnitt  ist abgearbeitet
+#define STOPBIT            6        // Ablauf stoppen
+#define FIRSTBIT           7
+
+
+#define OSZI_PULS_A        8
+#define OSZI_PULS_B        7
+
+
+#define THREAD_COUNT_BIT   0
+
+#define TIMERINTERVALL 128
+
+// Ramp
+#define RAMP_OK      1 // Ramp einschalten
+#define RAMPFAKTOR   2 // Verlaengerung der delayzeit
+#define RAMPZEIT     800 // Mindestdauer fuer Ramp
+
+#define RAMPSTARTBIT 1
+#define RAMPENDBIT 2
+#define RAMPEND0BIT 3 // Beginn der Endrampe
+#define RAMPOKBIT    7
+#define RAMPSCHRITT  1
+
+
+#define DEVICE_MILL  1
+#define DEVICE_JOY  2
+
+#define VORZEICHEN_X   0
+#define VORZEICHEN_Y   1
+
+
+// Zeichnen
+#define ACHSE0_BYTE_H   0
+#define ACHSE0_BYTE_L   1
+#define ACHSE0_START_BYTE_H   2
+#define ACHSE0_START_BYTE_L   3
+
+ 
+#define ACHSE1_BYTE_H   4
+#define ACHSE1_BYTE_L   5
+#define ACHSE1_START_BYTE_H   6
+#define ACHSE1_START_BYTE_L   7
+
+#define ACHSE2_BYTE_H   8
+#define ACHSE2_BYTE_L   9
+#define ACHSE2_START_BYTE_H   10
+#define ACHSE2_START_BYTE_L   11
+
+#define  ACHSE3_BYTE_H  12
+#define  ACHSE3_BYTE_L  13
+
+#define  INDEX_BYTE_H  18
+#define  INDEX_BYTE_L  19
+
+
+/*
+#define  HYP_BYTE_H  22 // Hypotenuse
+#define  HYP_BYTE_L 23
+
+
+#define  STEPS_BYTE_H  26
+#define  STEPS_BYTE_L  27
+*/
+
+
+
+#endif /* settings_h */
